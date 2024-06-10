0b2fabc90bf2574e7159651f6807f06868af73da
Grenish rai <ariestheracer@gmail.com>
2024-06-10 22:30:47 +0530

    chore: Add Cardigan images and Noise SVG to assets directory

---
+++ app/assets/index.ts
@@ -0,0 +1,4 @@
+export {default as Cardigan1 } from './Cardigan1.jpg'
+export {default as Cardigan2 } from './Cardigan2.jpg'
+export {default as Cardigan3 } from './Cardigan3.jpg'
+export {default as Noise} from './nnnoise.svg'
