7a7815a503bf2bbe4489e12bb980a9c92aac67d4
Grenish rai <ariestheracer@gmail.com>
2024-06-11 02:53:14 +0530

    feat: Add Navbar component for SnugNest landing page

---
+++ app/Pages/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <header className="p-2">
+      <nav className=" p-2">
+        <ul className="flex w-1/2 justify-between mx-auto">
+          <li>Home</li>
+          <li>Browse Catalogue</li>
+          <li>About Us</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
