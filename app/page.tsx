a1c9f3b84aee44ad630563d166ce6fe064b72a31
Grenish rai <ariestheracer@gmail.com>
2024-06-11 02:52:37 +0530

    feat: Add Home page component with Hero, Featured, and Products sections

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Featured from "./Pages/Featured";
+import Hero from "./Pages/Hero";
+import Products from "./Pages/Products";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Featured />
+      <Products />
+    </>
+  );
+}
