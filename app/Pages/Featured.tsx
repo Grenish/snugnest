a246470f09d88ed04b2180a8af90e67b27813381
Grenish rai <ariestheracer@gmail.com>
2024-06-11 23:39:01 +0530

    refactor: Update landing page layout and styles

--- app/Pages/Featured.tsx
+++ app/Pages/Featured.tsx
@@ -6,22 +6,24 @@ export default function Featured() {
   return (
     <div className="bg-white w-full min-h-screen flex flex-col">
       <Navbar />
-      <div className="px-56 flex justify-between items-center flex-grow">
-        <div className="w-1/2">
-          <h1 className="text-5xl Vaimek">
+      <div className="px-4 md:px-16 lg:px-56 flex flex-col lg:flex-row justify-between items-center flex-grow">
+        <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl Vaimek">
             Shop The Best <br /> For You
           </h1>
-          <p>
-            find the perfect cardigan that combines style, comfort, and quality.
+          <p className="mt-4">
+            Find the perfect cardigan that combines style, comfort, and quality.
             Our carefully crafted pieces are designed to keep you cozy and chic,
             whether at home or out and about. Shop the best for you and embrace
             unparalleled comfort with SnugNest.
           </p>
-          <button className="mt-2 p-2 bg-[#ffcdb2] hover:bg-[#ffb4a2] transition-colors duration-200 ease-in-out">Discover More</button>
+          <button className="mt-4 p-2 bg-[#ffcdb2] hover:bg-[#ffb4a2] transition-colors duration-200 ease-in-out">
+            Discover More
+          </button>
         </div>
         <Image
           src={Cardigan3.src}
-          className="h-[600px] pointer-event-none"
+          className="w-full lg:w-auto h-auto lg:h-[600px] pointer-events-none"
         />
       </div>
     </div>
