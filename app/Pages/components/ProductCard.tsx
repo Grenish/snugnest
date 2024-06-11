709c69c92f4400a2c15e7d5c49755c73eedaac0f
Grenish rai <ariestheracer@gmail.com>
2024-06-11 23:39:26 +0530

    refactor: Update ProductCard component layout and styles

--- app/Pages/components/ProductCard.tsx
+++ app/Pages/components/ProductCard.tsx
@@ -18,12 +18,12 @@ const ProductCard: React.FC<ProductCardProps> = ({
   rating,
 }) => {
   return (
-    <div className="border rounded-lg overflow-hidden w-[250px] h-[370px] bg-white flex flex-col">
-      <img src={image.src} alt={title} className="w-full h-64 object-cover" />
+    <div className="border rounded-lg overflow-hidden sm:w-[250px] w-[180px] sm:h-[370px] h-[350px] bg-white flex flex-col">
+      <img src={image.src} alt={title} className="w-full sm:h-64 h-56 object-cover" />
       <div className="p-4 flex flex-col justify-between flex-grow">
         <div>
           <div className="flex justify-between items-center">
-            <h2 className="font-bold text-lg mb-2 capitalize ">{title}</h2>
+            <h2 className="font-bold sm:text-lg text-sm mb-2 capitalize ">{title}</h2>
             {isEco && (
               <Tooltip content="Eco Friendly" className="text-xs">
                 <span className="text-green-500 cursor-pointer">
@@ -42,7 +42,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
           </div>
         </div>
         <div className="flex justify-between ">
-          <span className="flex items-center gap-2">
+          <span className="flex items-center gap-2 sm:text-md text-xs">
             <p>{rating ? rating : 0}</p>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -50,6 +50,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
               height="22"
               fill="gold"
               viewBox="0 0 256 256"
+              className="w-4 h-4 sm:w-6 sm:h-6 md:w-8 md:h-8 lg:w-6 lg:h-6 xl:w-6 xl:h-6 2xl:w-4 2xl:h-4"
             >
               <path
                 d="M229.06,108.79l-48.7,42,14.88,62.79a8.4,8.4,0,0,1-12.52,9.17L128,189.09,73.28,222.74a8.4,8.4,0,0,1-12.52-9.17l14.88-62.79-48.7-42A8.46,8.46,0,0,1,31.73,94L95.64,88.8l24.62-59.6a8.36,8.36,0,0,1,15.48,0l24.62,59.6L224.27,94A8.46,8.46,0,0,1,229.06,108.79Z"
@@ -58,7 +59,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
               <path d="M239.18,97.26A16.38,16.38,0,0,0,224.92,86l-59-4.76L143.14,26.15a16.36,16.36,0,0,0-30.27,0L90.11,81.23,31.08,86a16.46,16.46,0,0,0-9.37,28.86l45,38.83L53,211.75a16.38,16.38,0,0,0,24.5,17.82L128,198.49l50.53,31.08A16.4,16.4,0,0,0,203,211.75l-13.76-58.07,45-38.83A16.43,16.43,0,0,0,239.18,97.26Zm-15.34,5.47-48.7,42a8,8,0,0,0-2.56,7.91l14.88,62.8a.37.37,0,0,1-.17.48c-.18.14-.23.11-.38,0l-54.72-33.65a8,8,0,0,0-8.38,0L69.09,215.94c-.15.09-.19.12-.38,0a.37.37,0,0,1-.17-.48l14.88-62.8a8,8,0,0,0-2.56-7.91l-48.7-42c-.12-.1-.23-.19-.13-.5s.18-.27.33-.29l63.92-5.16A8,8,0,0,0,103,91.86l24.62-59.61c.08-.17.11-.25.35-.25s.27.08.35.25L153,91.86a8,8,0,0,0,6.75,4.92l63.92,5.16c.15,0,.24,0,.33.29S224,102.63,223.84,102.73Z"></path>
             </svg>
           </span>
-          <p className="text-gray-700">${price.toFixed(2)}</p>
+          <p className="text-gray-700 sm:text-md text-xs">${price.toFixed(2)}</p>
         </div>
       </div>
     </div>
