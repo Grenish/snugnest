a4f7c1eb5e23d445f160520a6b72505b807c0695
Grenish rai <ariestheracer@gmail.com>
2024-06-13 05:56:38 +0530

    refactor: Update Navbar component with responsive text size

--- app/Pages/Navbar.tsx
+++ app/Pages/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <header className="p-2">
       <nav className=" p-2">
-        <ul className="flex w-1/2 justify-between mx-auto">
+        <ul className="flex w-full sm:w-1/2 justify-between mx-auto text-xs sm:text-md">
           <li>Home</li>
           <li>Browse Catalogue</li>
           <li>About Us</li>
