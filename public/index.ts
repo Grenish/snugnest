4eb2ad0ff4e69343e8c4531f0780fe41cdbe736c
Grenish rai <ariestheracer@gmail.com>
2024-06-13 06:20:48 +0530

    refactor: Addedimages to public folder

---
+++ public/index.ts
@@ -0,0 +1,6 @@
+export {default as Cardigan1 } from './Cardigan1.jpg'
+export {default as Cardigan2 } from './Cardigan2.jpg'
+export {default as Cardigan3 } from './Cardigan3.jpg'
+export {default as Cardigan4 } from './Cardigan4.jpg'
+export {default as HNM1 } from './hnm1.jpeg'
+export {default as Noise} from './nnnoise.svg'
