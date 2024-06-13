47070ed568f23c5ec4c6f61819b0a6b8c3bc4b24
Grenish rai <ariestheracer@gmail.com>
2024-06-13 05:55:55 +0530

    refactor: Add new pages and components to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import FAQ from "./Pages/FAQ";
 import Featured from "./Pages/Featured";
+import Footer from "./Pages/Footer";
 import Hero from "./Pages/Hero";
 import Products from "./Pages/Products";
+import Testimonials from "./Pages/Testimonials";
 
 export default function Home() {
   return (
@@ -8,6 +11,9 @@ export default function Home() {
       <Hero />
       <Featured />
       <Products />
+      <Testimonials />
+      <FAQ />
+      <Footer />
     </>
   );
 }
